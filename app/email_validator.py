d773b251e93e2ebf5885f3b6ca788f9141684bee
unknown <kchambouin@gmail.com>
2024-06-05 11:26:40 +0200

    📦 NEW: nouveau test fonctionnalité

--- app/email_validator.py
+++ app/email_validator.py
@@ -2,9 +2,12 @@
 
 import re
 
+
+
 def is_valid_email(email):
     """ Valide si l'adresse e-mail donnée est conforme à un pattern simple. """
-    if re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', email):
+    if re.match(r'^[\w\.-]+[\+]?[\w\.-]*@[\w\.-]+\.[\w]+$', email):
         return True
     else:
         return False
+
